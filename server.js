a8aa783c233b2cbd2fc76cac93eac42a96f921b8
Haibin Zhu <ssspiderzzz@gmail.com>
2019-11-11 11:31:48 -0800

    fix server side handle user orders bug

--- server.js
+++ server.js
@@ -65,29 +65,36 @@ App.get("/api/data", (req, res) => {
     [req.cookies.email]
   )
     .then(userID => {
-      const login_user_id = userID.rows[0].id;
-      db.query(
-        `
-        SELECT * FROM orders WHERE user_id = $1
-        `,
-        [login_user_id]
-      ).then(data1 => {
-        let order_ids = [];
-        data1.rows.map(order => {
-          order_ids.push(order.id);
-        });
-        if (order_ids.length > 0) {
-          db.query(
-            `
-          SELECT * FROM items WHERE order_id IN (${order_ids})
+      console.log("Login user id:" + userID.rows[0].id);
+      if (userID.rows[0]) {
+        const login_user_id = userID.rows[0].id;
+        db.query(
           `
-          ).then(data2 => {
-            res.json({ orders: data1, items: data2 });
+          SELECT * FROM orders WHERE user_id = $1
+          `,
+          [login_user_id]
+        ).then(data1 => {
+          let order_ids = [];
+          data1.rows.map(order => {
+            order_ids.push(order.id);
           });
-        } else {
-          res.json({ orders: [], items: [] });
-        }
-      });
+          if (order_ids.length > 0) {
+            db.query(
+              `
+              SELECT * FROM items WHERE order_id IN (${order_ids})
+              `
+            ).then(data2 => {
+              res.json({ orders: data1, items: data2 });
+            });
+          } else {
+            // if user has not order history
+            res.json({ orders: [], items: [] });
+          }
+        });
+      } else {
+        // if user id is not found
+        res.json({ orders: [], items: [] });
+      }
     })
     .catch(err => console.log(err));
 });
