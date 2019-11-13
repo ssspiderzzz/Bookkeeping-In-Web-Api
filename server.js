a28790e34e4bb12f2b4c163ac5734dc1696a70e6
Haibin Zhu <ssspiderzzz@gmail.com>
2019-11-12 22:23:04 -0800

    change origin address

--- server.js
+++ server.js
@@ -34,9 +34,7 @@ App.use(BodyParser.json());
 App.use(cookieParser());
 App.use(Express.static("public"));
 const corsOptions = {
-  origin: [
-    "https://5dca5d7d270ae5000a65ef93--amazing-dubinsky-a6a649.netlify.com/"
-  ],
+  origin: ["https://amazing-dubinsky-a6a649.netlify.com/"],
   credentials: true,
   allowedHeaders: [
     "Content-Type",
